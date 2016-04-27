6179eaa2a760d953498d2da7189f993bc38d834f
yogaviswani <yogaviswani.kanduri@asu.edu>
2016-04-26 23:18:24 -0700

    updating the code

--- WebContent/Util/scripts.js
+++ WebContent/Util/scripts.js
@@ -1,5 +1,27 @@
 performActionFor = function(command){
-	if(command.toLowerCase().indexOf("Bluetooth")>-1){
-        //Do something
+	
+	if(final_transcript.toLowerCase().indexOf("stop")!=-1 || final_transcript.toLowerCase().indexOf("off")!=-1 ){
+		location.reload(true);
+		//setTimeout(function(){document.getElementById("start_button").click(); }, 3000);
+		
+	}
+	
+	else if(final_transcript.toLowerCase().indexOf("time")!=-1){
+                
+        input = '<div id="time_div"> <iframe src="http://www.worldtimeserver.com/current_time_in_US-AZ.aspx #time" scrolling="no" frameborder="no" style="position: relative; height: 100%; width: 100%;"></iframe></div>';
+        $('#speech_results').html(input);
+    	clearContent();
     } 
+	else if(final_transcript.toLowerCase().indexOf("radio")!=-1){		
+		 input = '<div id="time_div"> <iframe src="http://streema.com/radios/play/1" scrolling="yes" frameborder="no" style="position: relative; height: 100%; width: 100%;"></iframe></div>';
+	     $('#speech_results').html(input);
+	     clearContent();
+	}
+	
+}
+
+function clearContent(){
+	
+	setTimeout(function(){document.getElementById("start_button").click(); }, 3000);
+	document.getElementById("start_button").click();
 }
